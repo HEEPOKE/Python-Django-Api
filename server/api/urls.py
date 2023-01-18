ab1248c6143717a7ee098b1f356b82de325cc375
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2023-01-18 15:05:17 +0700

    :money_with_wings: Update

--- server/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import ItemList, ItemDetail, LocationDetail, LocationList
-
-urlpatterns = [
-    path('location/', LocationList.as_view()),
-    path('location/<int:pk>/', LocationDetail.as_view()),
-    path('item/', ItemList.as_view()),
-    path('item/<int:pk>/', ItemDetail.as_view()),
-]
