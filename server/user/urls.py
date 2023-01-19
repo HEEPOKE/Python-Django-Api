6bfe7e2072612f3a7e48c0289e29aa7ec6c6ab3e
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2023-01-19 09:43:14 +0700

    Update urls.py

--- server/user/urls.py
+++ server/user/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('list/', ListUser.as_view()),
-    path('create/', CreateUser.as_view()),
-    path('update/id/<int:pk>/', UpdateUser.as_view()),
-    path('delete/id/<int:pk>/', DeleteUser.as_view()),
+    path('list', ListUser.as_view()),
+    path('create', CreateUser.as_view()),
+    path('update/<int:pk>', UpdateUser.as_view()),
+    path('delete/<int:pk>', DeleteUser.as_view()),
 ]
