7fca21990237cba62d0abb52cdf15a27c7ce0af8
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2023-01-19 09:19:03 +0700

    Update urls.py

--- server/user/urls.py
+++ server/user/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('list', ListUser.as_view()),
+    path('list/', ListUser.as_view()),
     path('create/', CreateUser.as_view()),
     path('update/id/<int:pk>/', UpdateUser.as_view()),
     path('delete/id/<int:pk>/', DeleteUser.as_view()),
