18126de29fedd2b9cfbead86f4e6da30db5fb118
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2023-01-19 09:43:22 +0700

    Update views.py

--- server/user/views.py
+++ server/user/views.py
@@ -6,19 +6,27 @@ from .models import *
 
 class ListUser(generics.ListAPIView):
     queryset = User.objects.all()
-    serializers_class = UserSerializer
+
+    def get_serializer_class(self):
+        return UserSerializer
 
 
 class CreateUser(generics.CreateAPIView):
     queryset = User.objects.all()
-    serializers_class = UserSerializer
+
+    def get_serializer_class(self):
+        return UserSerializer
 
 
 class UpdateUser(generics.RetrieveUpdateAPIView):
     queryset = User.objects.all()
-    serializers_class = UserSerializer
+
+    def get_serializer_class(self):
+        return UserSerializer
 
 
 class DeleteUser(generics.DestroyAPIView):
     queryset = User.objects.all()
-    serializers_class = UserSerializer
+
+    def get_serializer_class(self):
+        return UserSerializer
